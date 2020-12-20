0604f753844cc4c146bf4bb921a70c53d3320933
Sarvesh Roshan <63512093+imroshan12@users.noreply.github.com>
2020-12-20 17:10:36 +0530

    Create hangman.py

---
+++ hangman.py
@@ -0,0 +1,327 @@
+import random
+import os
+guessed = []
+logo = ''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
+word_list = [
+'abruptly',
+'absurd',
+'abyss',
+'affix',
+'askew',
+'avenue',
+'awkward',
+'axiom',
+'azure',
+'bagpipes',
+'bandwagon',
+'banjo',
+'bayou',
+'beekeeper',
+'bikini',
+'blitz',
+'blizzard',
+'boggle',
+'bookworm',
+'boxcar',
+'boxful',
+'buckaroo',
+'buffalo',
+'buffoon',
+'buxom',
+'buzzard',
+'buzzing',
+'buzzwords',
+'caliph',
+'cobweb',
+'cockiness',
+'croquet',
+'crypt',
+'curacao',
+'cycle',
+'daiquiri',
+'dirndl',
+'disavow',
+'dizzying',
+'duplex',
+'dwarves',
+'embezzle',
+'equip',
+'espionage',
+'euouae',
+'exodus',
+'faking',
+'fishhook',
+'fixable',
+'fjord',
+'flapjack',
+'flopping',
+'fluffiness',
+'flyby',
+'foxglove',
+'frazzled',
+'frizzled',
+'fuchsia',
+'funny',
+'gabby',
+'galaxy',
+'galvanize',
+'gazebo',
+'giaour',
+'gizmo',
+'glowworm',
+'glyph',
+'gnarly',
+'gnostic',
+'gossip',
+'grogginess',
+'haiku',
+'haphazard',
+'hyphen',
+'iatrogenic',
+'icebox',
+'injury',
+'ivory',
+'ivy',
+'jackpot',
+'jaundice',
+'jawbreaker',
+'jaywalk',
+'jazziest',
+'jazzy',
+'jelly',
+'jigsaw',
+'jinx',
+'jiujitsu',
+'jockey',
+'jogging',
+'joking',
+'jovial',
+'joyful',
+'juicy',
+'jukebox',
+'jumbo',
+'kayak',
+'kazoo',
+'keyhole',
+'khaki',
+'kilobyte',
+'kiosk',
+'kitsch',
+'kiwifruit',
+'klutz',
+'knapsack',
+'larynx',
+'lengths',
+'lucky',
+'luxury',
+'lymph',
+'marquis',
+'matrix',
+'megahertz',
+'microwave',
+'mnemonic',
+'mystify',
+'naphtha',
+'nightclub',
+'nowadays',
+'numbskull',
+'nymph',
+'onyx',
+'ovary',
+'oxidize',
+'oxygen',
+'pajama',
+'peekaboo',
+'phlegm',
+'pixel',
+'pizazz',
+'pneumonia',
+'polka',
+'pshaw',
+'psyche',
+'puppy',
+'puzzling',
+'quartz',
+'queue',
+'quips',
+'quixotic',
+'quiz',
+'quizzes',
+'quorum',
+'razzmatazz',
+'rhubarb',
+'rhythm',
+'rickshaw',
+'schnapps',
+'scratch',
+'shiv',
+'snazzy',
+'sphinx',
+'spritz',
+'squawk',
+'staff',
+'strength',
+'strengths',
+'stretch',
+'stronghold',
+'stymied',
+'subway',
+'swivel',
+'syndrome',
+'thriftless',
+'thumbscrew',
+'topaz',
+'transcript',
+'transgress',
+'transplant',
+'triphthong',
+'twelfth',
+'twelfths',
+'unknown',
+'unworthy',
+'unzip',
+'uptown',
+'vaporize',
+'vixen',
+'vodka',
+'voodoo',
+'vortex',
+'voyeurism',
+'walkway',
+'waltz',
+'wave',
+'wavy',
+'waxy',
+'wellspring',
+'wheezy',
+'whiskey',
+'whizzing',
+'whomever',
+'wimpy',
+'witchcraft',
+'wizard',
+'woozy',
+'wristwatch',
+'wyvern',
+'xylophone',
+'yachtsman',
+'yippee',
+'yoked',
+'youthful',
+'yummy',
+'zephyr',
+'zigzag',
+'zigzagging',
+'zilch',
+'zipper',
+'zodiac',
+'zombie',
+]
+f = ["" for i in range(7)]
+f[0] = """
+
+   +---+
+   |   |
+       |
+       |
+       |
+       |
+
+"""
+
+f[1] = """
+
+   +---+
+   |   |
+   o   |
+       |
+       |
+       |
+
+"""
+f[2] = """
+
+   +---+
+   |   |
+   o   |
+    \  |
+       |
+       |
+
+"""
+f[3] = """
+
+   +---+
+   |   |
+   o   |
+  / \  |
+       |
+       |
+
+"""
+f[4] = """
+
+   +---+
+   |   |
+   o   |
+  /|\  |
+       |
+       |
+
+"""
+f[5] = """
+
+   +---+
+   |   |
+   o   |
+  /|\  |
+    \  |
+       |
+
+"""
+f[6] = """
+
+   +---+
+   |   |
+   o   |
+  /|\  |
+  / \  |
+       |
+
+"""
+print(logo)
+chosen_word = random.choice(word_list)
+print(f'Pssst, the solution is {chosen_word}.')
+display = ["_" for j in range(len(chosen_word))]
+life = 6
+while "_" in display:
+  guess = input("Guess a letter: ").lower()
+  os.system('cls')
+  if guess in guessed:
+    print(f"You have already chosen {guess}")
+    print(*display)
+    print(f[len(f) - life - 1])
+    print("-------------------------------")
+    continue
+  guessed.append(guess)
+  if guess in chosen_word:
+    for i in range(len(display)):
+      if guess == chosen_word[i]:
+        display[i] = guess
+  else:
+    print(f"You guessed {guess}. It's not in the word. You lose a life.")
+    life -= 1
+    if life == 0:
+      print("You lose.")
+  print(*display)
+  print(f[len(f) - life - 1])
+  print("-------------------------------")
+print("You won")
